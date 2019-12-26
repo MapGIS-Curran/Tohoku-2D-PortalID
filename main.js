9cd6b7bdd58aabdd3b4b9702c70ff23307f94ea7
MapGIS <9409101+MapGIS-Curran@users.noreply.github.com>
2019-12-26 01:51:26 -0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-require(["esri/views/MapView", "esri/WebMap"], function (MapView, WebMap) {
-  /************************************************************
-   * Creates a new WebMap instance. A WebMap must reference
-   * a PortalItem ID that represents a WebMap saved to
-   * arcgis.com or an on-premise portal.
-   *
-   * To load a WebMap from an on-premise portal, set the portal
-   * url with esriConfig.portalUrl.
-   ************************************************************/
+require(["esri/views/MapView", "esri/WebMap"], 
+function(MapView, WebMap) 
+  {
   var webmap = new WebMap({
     portalItem: {
       // autocasts as new PortalItem()
@@ -14,9 +8,7 @@ require(["esri/views/MapView", "esri/WebMap"], function (MapView, WebMap) {
     }
   });
 
-  /************************************************************
-   * Set the WebMap instance to the map property in a MapView.
-   ************************************************************/
+  // Set the WebMap instance to the map property in a MapView.
   var view = new MapView({
     map: webmap,
     container: "viewDiv"
